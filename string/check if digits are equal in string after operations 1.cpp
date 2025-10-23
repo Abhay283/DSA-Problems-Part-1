4e1ee675f2af65a22e5b4ddca2a07f91614bbfd8
Abhay <abhaygupta4235@gmail.com>
2025-10-23 13:37:04 +0530

    initial commit

--- string/check if digits are equal in string after operations 1.cpp
+++ string/check if digits are equal in string after operations 1.cpp
@@ -1,15 +1,33 @@
 #include<iostream>
 using namespace std;
+// class Solution {
+// public:
+//     bool hasSameDigits(string s) {
+//         while (s.size() > 2) {
+//             string res = "";
+//             for (int i = 0; i < s.size() - 1; i++) {
+//                 int sum = (s[i] - '0') + (s[i + 1] - '0');
+//                 res += (sum % 10) + '0';
+//             }
+//             s = res;
+//         }
+//         return s.size() == 2 && s[0] == s[1];
+//     }
+// };
+
 class Solution {
 public:
     bool hasSameDigits(string s) {
-        while (s.size() > 2) {
-            string res = "";
-            for (int i = 0; i < s.size() - 1; i++) {
-                int sum = (s[i] - '0') + (s[i + 1] - '0');
-                res += (sum % 10) + '0';
+        int i = 0;
+        string res = "";
+        while (s.size() > 2 && i < s.size() - 1) {
+            res += (s[i] + s[i + 1]) % 10;
+            i++;
+            if (i == s.size() - 1) {
+                s = res;
+                i = 0;
+                res = "";
             }
-            s = res;
         }
         return s.size() == 2 && s[0] == s[1];
     }
