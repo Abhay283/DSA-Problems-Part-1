01285cbe3d1538fc3569aad41079ef8c9cd6cc6b
Abhay <abhaygupta4235@gmail.com>
2025-11-27 09:58:40 +0530

    write the new code

---
+++ Array/keep.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == original) {
+                original = 2 * original;
+            }
+        }
+        return original;
+    }
+};
