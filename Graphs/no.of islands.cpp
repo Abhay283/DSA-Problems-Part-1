629d0f0aa0ee2fd7c4d59c25b054565ba2bc29d8
Abhay <abhaygupta4235@gmail.com>
2026-01-21 19:26:59 +0530

    write the new code

---
+++ Graphs/no.of islands.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+class solution{
+    private:
+    void bfs(int row,int col,vector<vector<int>>& vis,vector<vector<char>>& grid){
+        vis[row][col]=1;
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int delrow=-1;delrow<=1;delrow++){
+                for(int delcol=-1;delcol<=1;delcol++){
+                    int nrow=row+delrow;
+                    int ncol=col+delcol;
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='L' && !vis[nrow][ncol]){
+                        vis[nrow][ncol]=1;
+                        q.push({nrow,ncol});
+                    }
+                }
+            }
+        }
+    }
+    public:
+    int numIslands(vector<vector<char>>& grid){
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int cnt=0;
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(grid[row][col]=='L' && !vis[row][col]){
+                     cnt++;
+                     bfs(row,col,vis,grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<char>>grid(n,vector<char>(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>grid[i][j];
+        }
+    }
+    solution obj;
+    cout<<obj.numIslands(grid)<<endl;
+    return 0;
+}
